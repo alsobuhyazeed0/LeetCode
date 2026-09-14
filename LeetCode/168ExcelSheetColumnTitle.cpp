f806ff319d5f0719228ac8fceae28e8f7e1aad79
alsobuhyazeed0 <alsobuhyazeed0@gmail.com>
2026-09-14 17:03:34 +0300

    168ExcelSheetColumnTitle-01

--- LeetCode/168ExcelSheetColumnTitle.cpp
+++ LeetCode/168ExcelSheetColumnTitle.cpp
@@ -3,27 +3,11 @@ using namespace std;
 #include "Solution.h";
 
 string Solution::convertToTitle(int columnNumber) {
-	int x = 0;
 	string ans = "";
 
-	while (true) {
-		x = columnNumber % 26;
-		
-		if (x == 0) {
-			ans += 'Z';
-			x = 26;
-		}
-		else {
-			ans += 'A' + (x - 1);
-		}
-
-		if (columnNumber > 26) {
-			columnNumber = (columnNumber - x) / 26;
-		}
-		else {
-			break;
-		}
-
+	while (columnNumber) {
+		ans += 'A' + ((columnNumber-1) % 26);
+		columnNumber = (columnNumber - 1) / 26;
 	}
 
 	std::reverse(ans.begin(), ans.end());
