12f843d447bf3749ae8cf4b1c76833aae9d6e4f3
alsobuhyazeed0 <alsobuhyazeed0@gmail.com>
2026-09-17 00:21:45 +0300

    171ExcelSheetColumnNumber-01

--- LeetCode/171ExcelSheetColumnNumber.cpp
+++ LeetCode/171ExcelSheetColumnNumber.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int Solution::titleToNumber(string columnTitle) {
 	int ans = 0;
 	for (int i = 0; i < columnTitle.size(); i++) {
-		ans += pow(26, columnTitle.size() - i - 1) * ((columnTitle[i] - 64) % 27);
+		ans = ans * 26 + (columnTitle[i] - 'A' + 1);
 	}
 	return ans;
 }
